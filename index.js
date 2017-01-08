f52892879dad8540881a36ac073e666f894e1a9d
tianyawy <tianya_wy@hotmail.com>
2017-01-08 18:10:58 +0800

    [victoryw] revert the recompress

--- index.js
+++ index.js
@@ -11,11 +11,22 @@ imagemin(['images/*.{jpg,png}'], 'build/images', {
 });
 
 
-const imageminJpegRecompress = require('imagemin-jpeg-recompress');
+
 const imageminJpegtran = require('imagemin-jpegtran');
 imagemin(['images/*.{jpg,png}'], 'build2/images', {
     plugins: [
         imageminJpegtran({progressive: true})
+    ]
+}).then(files => {
+    console.log(files);
+//=> [{data: <Buffer 89 50 4e …>, path: 'build/images/foo.jpg'}, …]
+});
+
+const imageminJpegRecompress = require('imagemin-jpeg-recompress');
+imagemin(['images/*.{jpg,png}'], 'build3/images', {
+    plugins: [
+        imageminJpegtran({progressive: true}),
+        imageminJpegRecompress({min: 95, max:95})
     ]
 }).then(files => {
     console.log(files);
